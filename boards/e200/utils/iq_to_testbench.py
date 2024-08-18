fixed scaling, added output dir flag

boards/e200/utils/iq_to_testbench.py
@@ -5,45 +5,49 @@ import matplotlib.pyplot as plt
 import argparse
 import struct
 
-def convert_iq(filename=None, out=None, length=None, tone_freqs=None, fs=None, trim=0, scale=1, debug=0):
+def convert_iq(in_file=None, out_file=None, out_dir=None, length=None, tone_freqs=None, fs=None, trim=0, scale=1, debug=0):
     # Load file if given
-    if filename is not None:
-        IQ_in = np.fromfile(filename, dtype=np.complex64)
-        I_in = np.real(IQ_in)
-        Q_in = np.imag(IQ_in)
-    else:
-        filename = "tones.dat"
-        t = np.arange(0, 0.1, 1/fs)
-        IQ = np.zeros(t.size, dtype=np.complex128)
-        for f in tone_freqs:
-            IQ += 32767*np.exp(1j*2*np.pi*f*t)
-        I = np.real(IQ).astype(np.int16).tolist()
-        Q = np.imag(IQ).astype(np.int16).tolist()
+    if in_file is not None:
+        IQ_in = np.fromfile(in_file, dtype=np.complex64)
 
-    # Set output filename if given
-    if out is None:
-        out_filename = filename.split('/')[-1].split('.')[0]
+    # Otherwise, generate tones
     else:
-        out_filename = out
+        in_file = "tones.dat"
+        if length is None:
+            t = np.arange(0, 0.1, 1/fs)
+        else:
+            t = 0 + np.arange(0, length) * 1/fs
+        IQ_in = np.zeros(t.size, dtype=np.complex64)
+        for f in tone_freqs:
+            IQ_in += np.exp(1j*2*np.pi*f*t)
+    
+    I = np.real(IQ_in)
+    Q = np.imag(IQ_in)
     
     # Trim IQ file
     if length is None:
-        length = IQ.size
-        I = I_in[int(trim):]
-        Q = Q_in[int(trim):]
+        I = I[int(trim):]
+        Q = Q[int(trim):]
+        length = I.size
     else:
-        I = I_in[int(trim):int(trim)+length]
-        Q = Q_in[int(trim):int(trim)+length]
+        I = I[int(trim):int(trim)+length]
+        Q = Q[int(trim):int(trim)+length]
+
+    # Set output filename if given
+    if out_file is None:
+        out_filename = in_file.split('/')[-1].split('.')[0]
+    else:
+        out_filename = out_file
         
     # Convert to full-scale int16
     I = float_to_int16(I)
     Q = float_to_int16(Q)
 
     # Scale down
-    I = np.left_shift(I, 4)
-    Q = np.left_shift(Q, 4)
-    I = np.left_shift(I, scale)
-    Q = np.left_shift(Q, scale)
+    I = np.right_shift(I, 4)
+    Q = np.right_shift(Q, 4)
+    I = np.right_shift(I, scale)
+    Q = np.right_shift(Q, scale)
 
     # Append leading 0 for FPGA reset in testbench
     np.insert(I, 0, 0)
@@ -51,7 +55,9 @@ def convert_iq(filename=None, out=None, length=None, tone_freqs=None, fs=None, t
     
     # Debug plot
     if (debug):
+        plt.figure()
         plt.plot(I)
+        plt.title("Scaled int16")
         plt.show()
     
     # Write data to binary file for MATLAB/Python model
@@ -59,7 +65,7 @@ def convert_iq(filename=None, out=None, length=None, tone_freqs=None, fs=None, t
     IQ_interleaved[0::2] = I[:length]
     IQ_interleaved[1::2] = Q[:length]
     
-    IQ_filename = "tb_" + out_filename + ".ic16"
+    IQ_filename = out_dir + "/" + "tb_" + out_filename + ".ic16"
     IQ_outfile = open(IQ_filename, 'wb')
     IQ_outfile.write(IQ_interleaved)
     IQ_outfile.close()
@@ -69,8 +75,8 @@ def convert_iq(filename=None, out=None, length=None, tone_freqs=None, fs=None, t
     Q_txt = "\n".join(int16_to_hex_string(sample) for sample in Q)
 
     # Create hex output filenames
-    I_filename = "tb_" + out_filename + "_I.hex"
-    Q_filename = "tb_" + out_filename + "_Q.hex"
+    I_filename = out_dir + "/" + "tb_" + out_filename + "_I.hex"
+    Q_filename = out_dir + "/" + "tb_" + out_filename + "_Q.hex"
 
     # Write hex data
     I_file = open(I_filename, 'w')
@@ -80,7 +86,7 @@ def convert_iq(filename=None, out=None, length=None, tone_freqs=None, fs=None, t
     Q_file = open(Q_filename, 'w')
     Q_file.write(Q_txt)
     Q_file.close() 
-    print(f"Done converting {filename}")
+    print(f"Done converting {in_file}")
 
 
 def float_to_int16(floats):
@@ -88,7 +94,14 @@ def float_to_int16(floats):
     min = np.min(floats)
     max = np.max(floats)
     norm = (floats - min) / (max - min)
-    int16 = (np.round(norm * (2**15-1)) - (2**14)).astype(np.int16)
+
+    int16_max = 2**15-1
+    int16_min = -2**15
+    
+    scaled = norm * (int16_max - int16_min) + int16_min
+    clipped = np.clip(scaled, int16_min, int16_max)
+    int16 = clipped.astype(np.int16)
+    
     return int16
 
 
@@ -100,8 +113,9 @@ def int16_to_hex_string(value):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Converts interleaved complex short to hex for modelsim imput")
-    parser.add_argument('-f', '--filename', nargs='?', default=None, type=str, help="IQ file name")
-    parser.add_argument('-o', '--out', nargs='?', default=None, type=str, help="output file name")
+    parser.add_argument('-if', '--input_file', nargs='?', default=None, type=str, help="Input IQ file")
+    parser.add_argument('-of', '--output_file', nargs='?', default=None, type=str, help="output file name")
+    parser.add_argument('-od', '--output_dir', nargs='?', default=None, type=str, help="output directory")
     parser.add_argument('-p', '--parameters', nargs='?', default=None, type=str, help="Comma-separated list of tones, followed by the sample rate")
     parser.add_argument('-l', '--length', nargs='?', default=None, type=int, help="Number of samples to write")
     parser.add_argument('-t', '--trim', nargs='?', default=0, type=int, help="Number of leading samples to trim")
@@ -116,4 +130,4 @@ if __name__ == '__main__':
         tone_freqs = []
         fs = None
     
-    convert_iq(args.filename, args.out, args.length, tone_freqs, fs, args.trim, args.scale, args.debug)
+    convert_iq(args.input_file, args.output_file, args.output_dir, args.length, tone_freqs, fs, args.trim, args.scale, args.debug)